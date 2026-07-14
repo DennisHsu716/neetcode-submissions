010c7dd8f48e1f03f4ce756b463e8891293ddae0
Dennis Hsu <134403492+DennisHsu716@users.noreply.github.com>
2026-07-13 23:04:35 -0700

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,38 @@
+class TriesNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False 
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TriesNode()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+
+        for i in word:
+            if i not in node.children:
+                node.children[i] = TriesNode()
+            node = node.children[i]
+        node.end = True 
+
+    def search(self, word: str) -> bool:
+        def dfs(node:Optional[TreeNode], index):
+            if index == len(word):
+                return node.end
+            
+            ch = word[index]
+
+            if ch == ".":
+                for i in node.children.values():
+                    if dfs(i, index + 1):
+                        return True 
+                return False
+
+            if ch not in node.children:
+                return False 
+
+            return dfs(node.children[ch], index + 1)
+
+        return dfs(self.root, 0) 
