d74abae286fe35211a3f0dc2128a5d987cf8b41b
Dennis Hsu <134403492+DennisHsu716@users.noreply.github.com>
2026-07-13 17:13:59 -0700

    Add: design-twitter-feed - submission-0

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-0.py
@@ -0,0 +1,40 @@
+class Twitter:
+
+    def __init__(self):
+        self.time = 0
+        self.tweetmap = defaultdict(list)
+        self.followmap = defaultdict(set)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweetmap[userId].append((self.time, tweetId))
+        self.time += 1
+        
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        res = []
+        heap = []
+        self.followmap[userId].add(userId)
+
+        for followee in self.followmap[userId]:
+            index = len(self.tweetmap[followee]) -1
+            time, tweetId = self.tweetmap[followee][index] 
+            heapq.heappush(heap, (-time, tweetId, followee, index)) 
+
+        while heap and len(res) < 10:
+            time, tweetId, followee, index = heapq.heappop(heap)
+            res.append(tweetId)
+
+            if index > 0:
+                index -= 1
+                time, tweetId = self.tweetmap[followee][index]
+                heapq.heappush(heap)
+        
+        return res       
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followmap[followerId].add(followeeId)
+        
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.followmap[followerId].discard(followeeId)
+        
