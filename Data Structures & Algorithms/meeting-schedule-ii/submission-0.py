4ce56997d7d5181fa5454a139cdd4c5d9454a077
Dennis Hsu <134403492+DennisHsu716@users.noreply.github.com>
2026-07-16 00:19:00 -0700

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.py
@@ -0,0 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        intervals.sort(key=lambda x:x.start)
+        heap = []
+
+        for interval in intervals:
+            if heap:
+                if interval.start >= heap[0]:
+                    end = heapq.heappop(heap)
+
+            heapq.heappush(heap, interval.end)
+        return len(heap)
