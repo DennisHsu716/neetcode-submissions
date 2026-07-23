c4428aed85392b078b2a5897b813ea9526cd6a25
Dennis Hsu <134403492+DennisHsu716@users.noreply.github.com>
2026-07-22 17:14:45 -0700

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_step = 0
+
+        for i, num in enumerate(nums):
+            if i > max_step:
+                return False 
+            
+            max_step = max(max_step, i + num)
+        return True 
+
