6772a6d8b187a527643a99a2fdd5643639dccfc1
Dennis Hsu <134403492+DennisHsu716@users.noreply.github.com>
2026-07-23 22:50:45 -0700

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(start, path, current_sum):
+            if current_sum > target:
+                return 
+            
+            if current_sum == target:
+                res.append(path[:])
+                return 
+            
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue 
+                
+                path.append(candidates[i])
+                backtrack(i + 1, path, candidates[i] + current_sum)
+                path.pop()
+        
+        backtrack(0, [], 0)
+        return res 
