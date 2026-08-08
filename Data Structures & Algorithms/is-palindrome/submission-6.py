ed02183825f4137dbdd29a03798fc50d91d14c6d
Dennis Hsu <134403492+DennisHsu716@users.noreply.github.com>
2026-08-07 17:00:04 -0700

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        count = []
+        for i in s:
+            if i.isalnum():
+                count.append(i)
+        
+        left = 0
+        right = len(count) - 1
+
+
+        while left < right:
+            if count[left] != count[right]:
+                return False 
+            else:
+                left += 1
+                right -= 1
+        return True 
