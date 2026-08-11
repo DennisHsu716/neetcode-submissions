6001abe8d8bf3bc2e11c6138b976b2af7dcabf5d
Dennis Hsu <134403492+DennisHsu716@users.noreply.github.com>
2026-08-10 22:29:44 -0700

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        pre1 = nums[0]
+        pre2 = nums[1]
+
+        for i in range(2, len(nums)):
+            cur = max(pre1 + nums[i], pre2)
+
+            pre1 = pre2
+            pre2 = cur
+        return pre2
