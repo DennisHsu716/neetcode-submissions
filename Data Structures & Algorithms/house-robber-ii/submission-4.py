56dddcde3e52c7870161e88069e20a70063b983d
Dennis Hsu <134403492+DennisHsu716@users.noreply.github.com>
2026-09-02 13:53:00 -0700

    Add: house-robber-ii - submission-4

---
+++ Data Structures & Algorithms/house-robber-ii/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def helper(nums):
+            if len(nums) == 1:
+                return nums[0]
+            
+            pre1 = nums[0]
+            pre2 = max(nums[0], nums[1])
+
+            for i in range(2, len(nums)):
+                curr = max(pre2, nums[i] + pre1)
+                pre1 = pre2
+                pre2 = curr
+            return pre2
+        
+        if len(nums) == 1:
+            return nums[0]
+        
+        return max(helper(nums[1:]), helper(nums[:-1]))
