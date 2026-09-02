21990cd762d99c9c8b254e97d8849ddf6f56e484
Dennis Hsu <134403492+DennisHsu716@users.noreply.github.com>
2026-09-02 13:42:14 -0700

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        pre1 = 1
+        pre2 = 2
+
+        for i in range(3, n + 1):
+            curr = pre1 + pre2
+            pre1 = pre2
+            pre2 = curr
+        return pre2
