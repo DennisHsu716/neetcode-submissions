7f09f41a721adc6b6c2e16c17d69272f3424a41f
Dennis Hsu <134403492+DennisHsu716@users.noreply.github.com>
2026-09-02 14:49:22 -0700

    Add: count-squares - submission-2

---
+++ Data Structures & Algorithms/count-squares/submission-2.py
@@ -0,0 +1,40 @@
+class CountSquares:
+
+    def __init__(self):
+        self.point = {}
+
+    def add(self, point: List[int]) -> None:
+        x, y = point
+        self.point[(x, y)] = self.point.get((x, y), 0) + 1
+
+    def count(self, point: List[int]) -> int:
+        x, y = point 
+        res = 0
+
+        for (px, py) in self.point:
+            if py != y:
+                continue 
+            
+            if px == x:
+                continue 
+            
+            d = abs(x - px)
+
+            top_left = (x, y + d)
+            top_right = (px, y + d)
+        
+            botton_left = (x, y - d)
+            botton_right = (px, y - d)
+
+            res += (
+                self.point.get(top_left, 0) *
+                self.point.get(top_right, 0) *
+                self.point.get((px, py), 0)
+            )
+
+            res += (
+                self.point.get(botton_left, 0) * 
+                self.point.get(botton_right, 0) *
+                self.point.get((px, py), 0)
+            )
+        return res 
