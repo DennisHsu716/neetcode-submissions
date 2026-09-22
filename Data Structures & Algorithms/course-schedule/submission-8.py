37282084005f14bbf6ef025b129d18743cee472a
Dennis Hsu <134403492+DennisHsu716@users.noreply.github.com>
2026-09-21 21:32:38 -0700

    Add: course-schedule - submission-8

---
+++ Data Structures & Algorithms/course-schedule/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = [[] for _ in range(numCourses)]
+        for course, preq in prerequisites:
+            graph[preq].append(course)
+        
+        state = [0] * numCourses
+
+        def dfs(course):
+            if state[course] == 1:
+                return False 
+            
+            if state[course] == 2:
+                return True 
+            
+            state[course] = 1
+
+            for i in graph[course]:
+                if not dfs(i):
+                    return False 
+            
+            state[course] = 2
+            return True 
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return False 
+        return True 
